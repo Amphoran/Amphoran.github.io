45c010026d220f2aefb7c094ad6de3a3246655e5
Amphoran <78102553+Amphoran@users.noreply.github.com>
2023-06-22 21:26:42 -0400

    Add files via upload

--- exampleboxscript.js
+++ exampleboxscript.js
@@ -1,4 +1,3 @@
-/* from https://sebhastian.com/javascript-rotate-images/ */
 				let rotation1 = 0;
 				function rotateImg1() 
 				{
@@ -10,12 +9,14 @@
 					}
 					document.querySelector(".exampleboxheadercursoricon1").style.transform = `rotate(${rotation1}deg)`;
 				}
-				$("#examplebutton1").click(function()
+				let transitionIsHappening1 = false;
+				$("#examplebutton1").click(function() 
 				{
+					if (transitionIsHappening1) return;
+					transitionIsHappening1 = true;
+					setTimeout(() => transitionIsHappening1 = false, 500);
 					$("#examplecontent1").slideToggle(500);
 					rotateImg1();
-					
-					
 				});
 				let rotation2 = 0;
 				function rotateImg2() 
@@ -28,12 +29,14 @@
 					}
 					document.querySelector(".exampleboxheadercursoricon2").style.transform = `rotate(${rotation2}deg)`;
 				}
-				$("#examplebutton2").click(function()
+				let transitionIsHappening2 = false;
+				$("#examplebutton2").click(function() 
 				{
+					if (transitionIsHappening2) return;
+					transitionIsHappening2 = true;
+					setTimeout(() => transitionIsHappening2 = false, 500);
 					$("#examplecontent2").slideToggle(500);
 					rotateImg2();
-					
-					
 				});
 				let rotation3 = 0;
 				function rotateImg3() 
@@ -46,12 +49,14 @@
 					}
 					document.querySelector(".exampleboxheadercursoricon3").style.transform = `rotate(${rotation3}deg)`;
 				}
-				$("#examplebutton3").click(function()
+				let transitionIsHappening3 = false;
+				$("#examplebutton3").click(function() 
 				{
+					if (transitionIsHappening3) return;
+					transitionIsHappening3 = true;
+					setTimeout(() => transitionIsHappening3 = false, 500);
 					$("#examplecontent3").slideToggle(500);
 					rotateImg3();
-					
-					
 				});
 				let rotation4 = 0;
 				function rotateImg4() 
@@ -64,12 +69,14 @@
 					}
 					document.querySelector(".exampleboxheadercursoricon4").style.transform = `rotate(${rotation4}deg)`;
 				}
-				$("#examplebutton4").click(function()
+				let transitionIsHappening4 = false;
+				$("#examplebutton4").click(function() 
 				{
+					if (transitionIsHappening4) return;
+					transitionIsHappening4 = true;
+					setTimeout(() => transitionIsHappening4 = false, 500);
 					$("#examplecontent4").slideToggle(500);
 					rotateImg4();
-					
-					
 				});
 				let rotation5 = 0;
 				function rotateImg5() 
@@ -82,12 +89,14 @@
 					}
 					document.querySelector(".exampleboxheadercursoricon5").style.transform = `rotate(${rotation5}deg)`;
 				}
-				$("#examplebutton5").click(function()
+				let transitionIsHappening5 = false;
+				$("#examplebutton5").click(function() 
 				{
+					if (transitionIsHappening5) return;
+					transitionIsHappening5 = true;
+					setTimeout(() => transitionIsHappening5 = false, 500);
 					$("#examplecontent5").slideToggle(500);
 					rotateImg5();
-					
-					
 				});
 				let rotation6 = 0;
 				function rotateImg6() 
@@ -100,10 +109,12 @@
 					}
 					document.querySelector(".exampleboxheadercursoricon6").style.transform = `rotate(${rotation6}deg)`;
 				}
-				$("#examplebutton6").click(function()
+				let transitionIsHappening6 = false;
+				$("#examplebutton6").click(function() 
 				{
+					if (transitionIsHappening6) return;
+					transitionIsHappening6 = true;
+					setTimeout(() => transitionIsHappening6 = false, 500);
 					$("#examplecontent6").slideToggle(500);
 					rotateImg6();
-					
-					
 				});
